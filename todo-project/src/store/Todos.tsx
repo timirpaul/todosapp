3a350e59b0f66ac5e7bd70150a254ab30e60a6c3
Timir <timir-baran@dlglobalsb.onmicrosoft.com>
2024-09-11 18:25:03 +0530

    .

--- todo-project/src/store/Todos.tsx
+++ todo-project/src/store/Todos.tsx
@@ -1,12 +1,27 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState } from "react";
 
+export type TodosProviderProps = {
+  children: ReactNode;
+};
+export type Todo = {
+  id: string;
+  task: string;
+  completed: boolean;
+  createdAt: Date;
+};
+export type TodosContext = {
+  todos: Todo[];
+  handleAddtodo: (task: string) => void;
+};
 
-export const todosContext = createContext(null)
+export const todosContext = createContext<TodosContext | null>(null);
 
-export const TodosProvider =({children})=>{
-    return(
-        <todosContext.Provider>
-            {children}
-        </todosContext.Provider>
-    )
-}
+export const TodosProvider = ({ children }: TodosProviderProps) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAddTodo = (task) => {};
+  return (
+    <todosContext.Provider value={{ todo, handleAddTodo }}>
+      {children}
+    </todosContext.Provider>
+  );
+};
